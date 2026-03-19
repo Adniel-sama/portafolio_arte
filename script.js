6541f20ae575d6e092e929fc3ad5cf9caa62647d
Daniel C <danielc@live.cl>
2026-03-19 10:10:23 -0300

    acordeón

--- script.js
+++ script.js
@@ -39,3 +39,14 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+  const navbarCollapse = document.getElementById('navbarNav');
+
+  document.addEventListener('click', function (event) {
+    const isClickInside = navbarCollapse.contains(event.target);
+
+    // Si el menú está abierto y el click NO fue dentro del collapse, cerrarlo
+    if (!isClickInside && navbarCollapse.classList.contains('show')) {
+      const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+      bsCollapse.hide();
+    }
+  });
